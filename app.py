326c54bdfcadea4b0ecda7ab6ca473c0ee52fe21
Alex Jennings <aj0312@my.bristol.ac.uk>
2024-12-26 17:45:56 +0000

    feat: added main menu and back button

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 from assets import generate_user
-import random
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def main_menu():
+    return render_template('main-menu.html')
+
+@app.route('/user_table')
+def user_table():
     users = [generate_user() for _ in range(20)]
-    return render_template('index.html', users=users)
+    return render_template('user-table.html', users=users)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
