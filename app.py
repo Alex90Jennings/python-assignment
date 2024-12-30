e24628ba68cf78ffc1d2c779736680589a7978d5
Alex Jennings <aj0312@my.bristol.ac.uk>
2024-12-30 14:45:28 +0000

    feat: added user page

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from assets import generate_user
 
 app = Flask(__name__)
+users = [generate_user() for _ in range(20)]
 
 @app.route('/')
 def main_menu():
     return render_template('main-menu.html')
 
-@app.route('/user_table')
+@app.route('/users_table')
 def user_table():
-    users = [generate_user() for _ in range(20)]
-    return render_template('user-table.html', users=users)
+    return render_template('users-table.html', users=users)
+
+@app.route('/user-details')
+def user_details():
+    user_id = request.args.get('_id')
+    user = next((user for user in users if user['_id'] == user_id), None)
+    if user:
+        return render_template('user-details.html', user=user)
+    else:
+        return "User not found", 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
